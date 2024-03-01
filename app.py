33ef56148b4fca0098d2623bbe0bc223b2ca5632
Harsha Pulagam <120901279+Harsha-Pulagam@users.noreply.github.com>
2024-03-01 18:22:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,11 +14,30 @@ inference = Inference()
 async def home():
   return {"message": "Machine Learning service"}
 
+@router.get("/metadata")
+async def get_metadata():
+  try:
+    with open("metadata.sql", "r") as file:
+      metadata = file.read()
+    return {"metadata": metadata}
+  except Exception as e:
+    log.error("Failed to retrieve metadata")
+
+@router.put("/metadata")
+async def post_metadata(metadata: dict):
+  try:
+    text = metadata["metadata"]
+    with open("metadata.sql", "w") as file:
+      file.write(text)
+    return {"message": "Metadata updated successfully"}
+  except Exception as e:
+    log.error("Failed to update metadata")
+
 @router.post("/sqlcoder")
 async def data(data: dict):
   try:
     input_text = data["text"]
-    res = inference.sql_generator(input_text)
+    res = inference.generate_query(input_text)
     return res
   except Exception as e:
     log.error("Something went wrong")
@@ -27,3 +46,4 @@ app.include_router(router)
 
 if __name__ == "__main__":
   uvicorn.run("app:app", reload=True,port =8051,host="0.0.0.0")
+
